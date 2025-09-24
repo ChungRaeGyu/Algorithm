ab3378f0bdde5cf38a559a413e025e9b6934775e
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-09-24 11:01:48 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.61 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Array.Sort(numbers);
+        int max = numbers[numbers.Length-1];
+        int max2 = numbers[numbers.Length-2];
+
+        return max*max2;
+    }
+}
