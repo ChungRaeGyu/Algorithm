6a8cfd78d78a46c48413e68f960d7a5123c1a165
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-09-30 09:36:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.28 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        int sum = 0;
+        int temp = x;
+        while(temp!=0){
+            sum+=temp%10;
+            temp/=10;
+        }
+        return answer = x%sum==0?true:false;
+    }
+}
