2e0fef6e8a609ebf69c8562ae4e6770b11be98c5
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-09-30 20:41:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 52.78 ms, Memory: 47.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        
+        
+        for(int i=1;i<=n;i++){
+            answer += i%2==1?"수":"박";
+        }
+        return answer;
+    }
+}
