73b2276bf732cc99a0785e86027ff3e9b4dad79c
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-09-30 17:39:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.25 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -0,0 +1,14 @@
+using System;
+public class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long sqrt =(long)Math.Sqrt(n);
+        
+        if(sqrt*sqrt == n){
+            answer = (sqrt+1) * (sqrt+1);
+        }else{
+            answer = -1;
+        }
+        return answer;
+    }
+}
