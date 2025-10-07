d3e279b373bda4e0c75d45877c0df72aff5594c9
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-10-07 10:42:58 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.18 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2) {
+        int x = arr1.GetLength(0);
+        int y= arr1.GetLength(1);
+        int[,] answer = new int[x,y];
+        
+        for(int i=0; i<x;i++){
+            for(int j=0; j<y;j++){
+                answer[i,j] = arr1[i,j]+arr2[i,j];
+            }
+        }
+        
+        return answer;
+    }
+}
