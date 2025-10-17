c35792b7b034702c318c4ede3ba9def7ba31d643
Chung Rae Gyu <125470068+ChungRaeGyu@users.noreply.github.com>
2025-10-17 17:27:48 +0900

    [level 1] Title: 콜라 문제, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int temp = 0;
+        while(n>=a){
+            temp = n/a*b;
+            answer +=temp;
+            n = n%a+temp;
+        }
+        return answer;
+    }
+}
